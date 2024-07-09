a2112b038cba0068017b607877e4b7359872f683
Bashur Viachaslau <bashvime@gmail.com>
2024-07-10 00:40:07 +0300

    первое задание не до конца

---
+++ HW12delegatsAndCollback/HW12delegatsAndCollback/HW12delegatsAndCollback/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12delegatsAndCollback
+//
+//  Created by Вячеслав Башур on 09/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
