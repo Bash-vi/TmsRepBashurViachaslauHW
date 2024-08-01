3925bf3b0e03f8afd93dd29cef3f1dc064cc9a74
Bashur Viachaslau <bashvime@gmail.com>
2024-08-02 02:04:32 +0300

    готово

--- HW13UITableView/HW13UITableView/HW13UITableView/ViewController.swift
+++ HW13UITableView/HW13UITableView/HW13UITableView/ViewController.swift
@@ -7,11 +7,39 @@
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class Value1TableViewCell: UITableViewCell{
+    required override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Value2TableViewCell: UITableViewCell{
+    required override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value2, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class MyTableViewCell: UITableViewCell {
     private lazy var label = UILabel()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
+    }
     init(){
         super.init(style: .default, reuseIdentifier: nil)
+        setupCell()
+    }
+    
+    func setupCell() {
         contentView.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -19,18 +47,31 @@ class TableViewCell: UITableViewCell {
             label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
         ])
     }
+    
     func setup(textLab: String) {
         label.text = textLab
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        label.text = nil
+    }
 }
+
+struct Identifire {
+    static let myCell = "myCell"
+    static let cellValue1 = "cellValue1"
+    static let cellValue2 = "cellValue2"
+    static let cellDefault = "cellDefault"
+}
+
 
 class ViewController: UIViewController {
     lazy var tableView = UITableView()
-    
-    
+    let arrayProg = ["PHP", "JavaScript", "Java", "Python", "C++", "С#", "Visual Basic", "SQL", "Golang", "Assembler"]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
@@ -44,7 +85,10 @@ class ViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-        
+        tableView.register(MyTableViewCell.self, forCellReuseIdentifier: Identifire.myCell)
+        tableView.register(Value1TableViewCell.self, forCellReuseIdentifier: Identifire.cellValue1)
+        tableView.register(Value2TableViewCell.self, forCellReuseIdentifier: Identifire.cellValue2)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Identifire.cellDefault)
     }
 }
 
@@ -56,6 +100,7 @@ extension ViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
     }
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return switch section {
         case 0: "Задание 1"
@@ -64,12 +109,13 @@ extension ViewController: UITableViewDataSource {
         default: ""
         }
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
-            return 20
-        case 1:
             return 10
+        case 1:
+            return arrayProg.count
         case 2:
             return 20
         default:
@@ -78,10 +124,29 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell1 = TableViewCell()
-        cell1.setup(textLab: "Ячейка:\(indexPath.row)")
-        return cell1
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Identifire.myCell, for: indexPath) as? MyTableViewCell
+            cell?.setup(textLab: "Ячейка номер: \(indexPath.row)")
+            return cell ?? UITableViewCell()
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: Identifire.cellValue1, for: indexPath)
+            cell.textLabel?.text = "Язык:"
+            cell.detailTextLabel?.text = arrayProg[indexPath.row]
+            return cell
+        case 2:
+            if indexPath.row <= 9 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: Identifire.cellDefault, for: indexPath)
+                cell.textLabel?.text = arrayProg[indexPath.row]
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: Identifire.cellValue2, for: indexPath)
+                cell.textLabel?.text = "секция \(indexPath.section)"
+                cell.detailTextLabel?.text = "ячейка \(indexPath.row)"
+                return cell
+            }
+        default:
+            return UITableViewCell()
+        }
     }
-    
-    
 }
