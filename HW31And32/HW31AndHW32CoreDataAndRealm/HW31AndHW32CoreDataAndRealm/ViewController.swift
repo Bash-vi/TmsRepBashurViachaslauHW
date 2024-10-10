6f9e669cbe8fff2255e8312f2a28a978cd642ee5
Bashur Viachaslau <bashvime@gmail.com>
2024-10-10 12:17:23 +0300

    создал проект

---
+++ HW31And32/HW31AndHW32CoreDataAndRealm/HW31AndHW32CoreDataAndRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW31AndHW32CoreDataAndRealm
+//
+//  Created by Вячеслав Башур on 10/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
