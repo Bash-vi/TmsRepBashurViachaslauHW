b014a475adb280d78fa4c3e8bfd9a5588ddbe5f6
Bashur Viachaslau <bashvime@gmail.com>
2024-10-14 00:41:55 +0300

    done

---
+++ HW33APNSAndSecurity/HW33APNSAndSecurity/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  HW33APNSAndSecurity
+//
+//  Created by Вячеслав Башур on 13/10/2024.
+//
+
+import Foundation
+
+enum Constant {
+    static let indent: CGFloat = 20
+    static let spacing: CGFloat = 5
+    static let widthSubtitle: CGFloat = 150
+}
