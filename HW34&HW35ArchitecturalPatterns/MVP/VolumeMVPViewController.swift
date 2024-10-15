857ac97a7a1fbfc740ca4d24453c822c9e775658
Bashur Viachaslau <bashvime@gmail.com>
2024-10-16 01:43:08 +0300

    MVP+

---
+++ HW34&HW35ArchitecturalPatterns/MVP/VolumeMVPViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VolumeMVPViewController.swift
+//  HW34&HW35ArchitecturalPatterns
+//
+//  Created by Вячеслав Башур on 15/10/2024.
+//
+
+import UIKit
+
+class VolumeMVPViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HW34&HW35ArchitecturalPatterns/MVP/volumeMVPModel.swift
@@ -0,0 +1,8 @@
+//
+//  volumeMVPModel.swift
+//  HW34&HW35ArchitecturalPatterns
+//
+//  Created by Вячеслав Башур on 15/10/2024.
+//
+
+import Foundation
