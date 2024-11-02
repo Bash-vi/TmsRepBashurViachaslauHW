aeb1e834ee42418d6f2bbab6f0228c3263159f48
Bashur Viachaslau <bashvime@gmail.com>
2024-11-03 01:05:26 +0300

    доделать визуал и ячейку

---
+++ HW37Person2.0Localization/HobbiesViewController/HobbiesInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  HobbiesInteractor.swift
+//  HW37Person2.0Localization
+//
+//  Created by Вячеслав Башур on 02/11/2024.
+//
+
+import Foundation
---
+++ HW37Person2.0Localization/HobbiesViewController/HobbiesPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  HobbiesPresenter.swift
+//  HW37Person2.0Localization
+//
+//  Created by Вячеслав Башур on 02/11/2024.
+//
+
+import Foundation
